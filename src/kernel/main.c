947988801d2f315c74f686635bb4d8b1bb2224b3
AirGuanZ <248283056@qq.com>
2018-04-03 20:39:58 +0800

    物理页分配与释放测试

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -20,14 +20,20 @@ int main(void)
     intr_function[INTR_NUMBER_CLOCK] = pretend_to_be_a_scheduler;
     
     char intbuf[50];
+    uint32_t pages[80];
+    
     uint32_to_str(get_free_phy_page_count(), intbuf);
     put_str(intbuf); put_char('\n');
 
-    uint32_t page = alloc_phy_page(true);
+    for(int i = 0;i != 80; ++i)
+        pages[i] = alloc_phy_page(true);
+
     uint32_to_str(get_free_phy_page_count(), intbuf);
     put_str(intbuf); put_char('\n');
 
-    free_phy_page(page);
+    for(int i = 0;i != 80; ++i)
+        free_phy_page(pages[i]);
+
     uint32_to_str(get_free_phy_page_count(), intbuf);
     put_str(intbuf); put_char('\n');
 
