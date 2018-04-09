6a4dcb11bb7365f54e142f90772b98b056b29b13
AirGuanZ <248283056@qq.com>
2018-04-09 19:07:21 +0800

    Thread control block和thread kernel stack

---
+++ src/lib/freelist.c
@@ -0,0 +1,35 @@
+#include <lib/freelist.h>
+#include <lib/intdef.h>
+
+#define FL ((struct freelist_node*)handle)
+
+struct freelist_node
+{
+    struct freelist_node *next;
+};
+
+void init_freelist(freelist_handle *handle)
+{
+    FL->next = NULL;
+}
+
+void add_freelist(freelist_handle *handle, void *mem_zone)
+{
+    struct freelist_node *node = (struct freelist_node*)mem_zone;
+    node->next = FL->next;
+    FL->next = node;
+}
+
+bool is_freelist_empty(freelist_handle *handle)
+{
+    return FL->next != NULL;
+}
+
+void *fetch_freelist(freelist_handle *handle)
+{
+    if(!FL->next)
+        return NULL;
+    void *rt = FL->next;
+    FL->next = FL->next->next;
+    return rt;
+}
