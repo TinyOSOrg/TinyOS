98894cd3e794fdb3c8463eeaac39e544c55448f8
BlackCatWithWhiteBackground <2054035334@qq.com>
2018-05-09 18:27:21 +0800

    mmmm

--- src/kernel/drive_disk/drive_disk.c
+++ src/kernel/drive_disk/drive_disk.c
@@ -2,27 +2,42 @@
 #include<shared/intdef.h>
 #include<kernel/memory/phy_mem_man.h>
 #include<shared/bool.h>
-#include<drive_disk/drive_disk.h>
 #include<kernel/console/print.h>
+
 /*
-    set_buffer_addr函数仅在main.c函数开始时被调用一次;
-    is_busy、is_wrong和one_sec_data_carry为内部函数,不要在其他文件调用此函数。
+    任务队列；
+    检查硬盘是否正忙：
+    {
+        如果忙，将自己添加到队列中并阻塞自己；
+        如果不忙，将相关控制命令提交给磁盘，之后阻塞自己，等待中断唤醒自己；
+    }
+    传输数据完成后，检查阻塞队列：
+    {
+        若阻塞队列不为空，则唤醒阻塞队列队头的一个线程，否则read_disk或write_disk函数
+        完成。
+    }
+    注意事项：
+        磁盘读写必须是互斥的；
+    }
+    问题：
+    {
+        为什么是线程而不是进程阻塞？
+        磁盘完成后会自动产生相应的中断号？
+        为什么是循环链表？侵入式链表？
+        如何将现在的线程添加到阻塞队列？
+        如何将现在的线程从阻塞队列唤醒？
+        如何检查磁盘当前是否可以传输数据？
+    }
 */
-static uint16_t* buffer_addr;
-static void one_sec_data_carry(uint16_t* src, uint16_t* dst);
-static bool is_busy(void);
+
+/*   
+    is_wrong为内部函数,不要在其他文件调用此函数。
+*/
 static bool is_wrong(void);
-//set_buffer_addr函数仅在main.c函数开始时被调用一次
-static void set_buffer_addr(void);
 
-static void set_buffer_addr(void)
+bool write_disk(uint16_t* main_memory, uint32_t disk)
 {
-    buffer_addr = (uint16_t*)alloc_static_kernel_mem(512, 2);
-}
-
-
-bool missing_page_write_disk(uint16_t* main_memory, uint32_t disk)
-{
+    //首先检查硬盘是否正忙
     uint8_t low_8bits, mid_8bits, high_8bits, highest_8bits, cnt_sec = 1;
     low_8bits = disk;
     mid_8bits = disk >> 8;
@@ -39,11 +54,7 @@ bool missing_page_write_disk(uint16_t* main_memory, uint32_t disk)
     _out_byte_to_port(0x1f6, highest_8bits);
     //command:请求硬盘写
     _out_byte_to_port(0x1f7, 0x30);
-    //判断硬盘控制器是否已准备好
-    while(is_busy())
-    {
-        ;
-    }
+    //阻塞自己等待中断
     //若出错，则返回flase
     if(is_wrong())
     {
@@ -59,7 +70,7 @@ bool missing_page_write_disk(uint16_t* main_memory, uint32_t disk)
     return true;    
 }
 
-bool missing_page_read_disk(uint32_t disk, uint16_t* main_memory)
+bool read_disk(uint32_t disk, uint16_t* main_memory)
 {
     uint8_t low_8bits, mid_8bits, high_8bits, highest_8bits, cnt_sec = 1;
     low_8bits = disk;
@@ -77,11 +88,8 @@ bool missing_page_read_disk(uint32_t disk, uint16_t* main_memory)
     _out_byte_to_port(0x1f6, highest_8bits);
     //command:请求硬盘读
     _out_byte_to_port(0x1f7, 0x20);
-    //判断硬盘是否准备好
-    while(is_busy())
-    {
-        ;
-    }
+    //阻塞自己等待硬盘准备好
+
     //若出错，则返回flase
     if(is_wrong())
     {
@@ -97,33 +105,6 @@ bool missing_page_read_disk(uint32_t disk, uint16_t* main_memory)
     return true;  
 }
 
-bool normal_read_disk(uint32_t disk, uint16_t* main_memory)
-{
-    if(missing_page_read_disk(disk, buffer_addr) == false)
-        return false;
-    one_sec_data_carry(buffer_addr, main_memory);
-    return true;
-}
-
-bool normal_write_disk(uint16_t* main_memory, uint32_t disk)
-{
-    one_sec_data_carry(main_memory, buffer_addr);  
-    if(missing_page_write_disk(buffer_addr, disk) == false)
-        return false;  
-    return true;
-}
-
-static bool is_busy(void)
-{
-    uint8_t data_from_0x1f7, intermediate_variable;
-    data_from_0x1f7 = _in_byte_from_port(0x1f7);
-    intermediate_variable = data_from_0x1f7 & 0x08;
-    if(intermediate_variable == 0x08)
-        return false;
-    else   
-        return true; 
-}
-
 static bool is_wrong(void)
 {
     uint8_t stat;
@@ -132,17 +113,4 @@ static bool is_wrong(void)
         return true;
     else
         return false;
-}
-
-static void one_sec_data_carry(uint16_t* src, uint16_t* dst)
-{
-    for(size_t i = 0; i < 256; ++i)
-    {
-        *dst++ = *src++;
-    }
-}
-
-void init_disk_drive(void)
-{
-    set_buffer_addr();
 }
