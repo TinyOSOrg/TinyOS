03fcbf1c761452e2021fe487a060c1477561f56b
AirGuanZ <248283056@qq.com>
2018-05-29 20:47:12 +0800

    扩展测试用elf程序

--- src/applications/elf_tester/main.c
+++ src/applications/elf_tester/main.c
@@ -12,7 +12,24 @@ int main()
 {
     printf("Hello, elf!\n");
 
-    while(1)
-        yield_cpu();
+    register_char_msg();
+    register_key_msg();
+
+    while(true)
+    {
+        wait_for_sysmsg();
+        struct sysmsg msg;
+        while(peek_sysmsg(SYSMSG_SYSCALL_PEEK_OPERATION_REMOVE, &msg))
+        {
+            if(msg.type == SYSMSG_TYPE_CHAR)
+                put_char(get_chmsg_char(&msg));
+            else if(msg.type == SYSMSG_TYPE_KEYBOARD)
+            {
+                if(get_kbmsg_key(&msg) == VK_ESCAPE)
+                    return 0;
+            }
+        }
+    }
+
     return 0;
 }
